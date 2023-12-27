753572a131768894af8ed838e3144a8888ee5dc5
NataFilina <93826032+NataFilina@users.noreply.github.com>
2023-12-27 03:14:00 +0000

    Deploying to gh-pages from @ NataFilina/goit-react-hw-05-movies@751ac05d88d39dc27bbcdf2781aa996aaa53e57a 🚀

---
+++ static/js/805.5c5cc0e2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{805:function(e,r,n){n.r(r);var t=n(87),o=n(184);r.default=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("p",{children:"Sorry, page is not found"}),(0,o.jsx)(t.rU,{to:"/",children:"Go home "})]})}}}]);
+//# sourceMappingURL=805.5c5cc0e2.chunk.js.map
